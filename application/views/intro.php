e0e9fdcf296c31255d301a6818ac0188f92975d5
cqc <1016903103@qq.com>
2015-05-04 16:41:04 +0800

    change img

--- application/views/intro.php
+++ application/views/intro.php
@@ -4,20 +4,20 @@
 		<div class="row">
 			<div class="col-sm-4 col-xs-12">
 				<div id="intro1" class="intro-item">
-					<i class="fa fa-twitter icon1"></i>
-					<p>李政军演唱会李政军演唱会李政军演唱会</p>
+					<img src="<?php echo base_url();?>images/icon1.jpg">
+					<p>山东大学学生会</p>
 				</div>
 			</div>
 			<div class="col-sm-4 col-xs-12">
 				<div id="intro2" class="intro-item">
-					<i class="fa fa-facebook icon2"></i>
-					<p>李政军演唱会李政军演唱会李政军演唱会</p>
+					<img src="<?php echo base_url();?>images/icon2.jpg">
+					<p>中国共青团</p>
 				</div>
 			</div>
 			<div class="col-sm-4 col-xs-12">
 				<div id="intro3" class="intro-item">
-					<i class="fa fa-pinterest icon3"></i>
-					<p>李政军演唱会李政军演唱会李政军演唱会</p>
+					<img src="<?php echo base_url();?>images/icon3.jpg">
+					<p>大乘五蕴文化传媒公司</p>
 				</div>
 			</div>
 		</div>
